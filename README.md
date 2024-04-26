2eceb61643596b001830f20ba1f2dd8b1af9cdcd
Alex Grant <alex@localnerve.com>
2024-04-26 01:09:13 -0400

    download naming, data volume, test action, readme update

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # gigaohmbio-docker
+
+> A docker solution to download, transcribe videos for gigaohmbiological
+
+## Download
+The download docker container gets the latest video url from twitch, downloads it, transcodes it, and writes it to an output volume.
+
+Commands to run isolated (from project directory):
+```
+# build the image
+docker build -t 'gigaohmbio-download' -f Dockerfile-download .
+
+# run the image in a container of the same name
+docker run --name=gigaohmbio-download -v ./data:/home/pn/app/data 'gigaohmbio-download'
+```
+
+## Transcribe
+The transcription docker container reads an input audio file, runs whisper, and writes the output to an output volume.
+
+...WIP...
---
+++ entrypoint-download.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eu
+
+OUTPUT_FORMAT=m4a
+DEBUG=get-attribute npx -y @localnerve/get-attribute --url=https://m.twitch.tv/gigaohmbiological --selector='a[href^="/videos"]' --attribute=href --useprop=true >$GIGAOHMBIO_URL 2>$GIGAOHMBIO_LOG
+
+if [ $? -eq 0 -a `cat $GIGAOHMBIO_URL | wc -c` -gt 0 ]; then
+  twitch-dl download --format $OUTPUT_FORMAT --overwrite --quality audio_only --output {title_slug}.{format} `cat $GIGAOHMBIO_URL` >$GIGAOHMBIO_DL 2>$GIGAOHMBIO_LOG
+else
+  echo "Failed to get latest video url" >$GIGAOHMBIO_LOG
+  exit 1
+fi
+
+if [ $? -eq 0 ]; then
+  DOWNLOAD_RESULT=`tail -1 $GIGAOHMBIO_DL`
+  AUDIO_FILE=`echo $DOWNLOAD_RESULT | awk '{print $2}'`
+  cp `pwd`/$AUDIO_FILE $BINDMOUNT_VOL >$GIGAOHMBIO_LOG 2>&1
+  echo "DOWNLOAD_OK" >$GIGAOHMBIO_LOG
+else
+  echo "Failed to download video" >$GIGAOHMBIO_LOG
+  exit 2
+fi
