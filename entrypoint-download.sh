b641c92dd75ce239ac59734f8a8afa673a4c7c49
Alex Grant <alex@localnerve.com>
2024-04-27 14:05:34 -0400

    add npx debug

--- entrypoint-download.sh
+++ entrypoint-download.sh
@@ -4,6 +4,10 @@ set -eu
 
 echo "@@@ DEBUG 1"
 
+npx -y cowsay debug
+
+echo "@@@ DEBUG 1.5"
+
 OUTPUT_FORMAT=m4a
 DEBUG=get-attribute npx -y @localnerve/get-attribute --url=https://m.twitch.tv/gigaohmbiological --selector='a[href^="/videos"]' --attribute=href --useprop=true >$GIGAOHMBIO_URL 2>$GIGAOHMBIO_LOG
 
