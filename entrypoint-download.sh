14d7872ff8ca1912f317fa5dad3bdd22bfae022f
Alex Grant <alex@localnerve.com>
2024-04-26 23:55:00 -0400

    update exit codes

--- entrypoint-download.sh
+++ entrypoint-download.sh
@@ -9,7 +9,7 @@ if [ $? -eq 0 -a `cat $GIGAOHMBIO_URL | wc -c` -gt 0 ]; then
   twitch-dl download --format $OUTPUT_FORMAT --overwrite --quality audio_only --output {title_slug}.{format} `cat $GIGAOHMBIO_URL` >$GIGAOHMBIO_DL 2>>$GIGAOHMBIO_LOG
 else
   echo "Failed to get latest video url" >>$GIGAOHMBIO_LOG
-  exit 1
+  exit 4121
 fi
 
 if [ $? -eq 0 ]; then
@@ -20,9 +20,9 @@ if [ $? -eq 0 ]; then
     echo "DOWNLOAD_OK" >>$GIGAOHMBIO_LOG
   else
     echo "DOWNLOAD_FAILED" >>$GIGAOHMBIO_LOG
-    exit 3
+    exit 4123
   fi
 else
   echo "Failed to download video" >>$GIGAOHMBIO_LOG
-  exit 2
+  exit 4122
 fi
