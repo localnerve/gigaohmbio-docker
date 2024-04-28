a4b6219d4a5a11745df55964fd0c53a70b76a2f6
Alex Grant <alex@localnerve.com>
2024-04-27 23:13:24 -0400

    add launchargs, test full get-attribute

--- entrypoint-download.sh
+++ entrypoint-download.sh
@@ -4,15 +4,27 @@ set -eu
 
 echo "@@@ DEBUG 1"
 
-npx -y cowsay debug
-
-echo "@@@ DEBUG 1.5"
-
 OUTPUT_FORMAT=m4a
-DEBUG=cli,get-attribute npx -y @localnerve/get-attribute --url=https://m.twitch.tv/gigaohmbiological --selector='a[href^="/videos"]' --attribute=href --useprop=true
+CHROME_LAUNCHARGS=$(cat <<-EOF
+  "headless": true,
+  "args": [
+    "--no-sandbox",
+    "--disable-setuid-sandbox"
+  ]
+EOF
+
+)
+
+DEBUG=cli,get-attribute npx -y @localnerve/get-attribute \
+  --url=https://m.twitch.tv/gigaohmbiological\
+  --selector='a[href^="/videos"]'\
+  --attribute=href\
+  --useprop=true\
+  --launchargs="{$CHROME_LAUNCHARGS}"
+
+exit 0
 
 echo "@@@ DEBUG 2"
-exit 0
 
 if [ $? -eq 0 -a `cat $GIGAOHMBIO_URL | wc -c` -gt 0 ]; then
   twitch-dl download --format $OUTPUT_FORMAT --overwrite --quality audio_only --output {title_slug}.{format} `cat $GIGAOHMBIO_URL` >$GIGAOHMBIO_DL 2>>$GIGAOHMBIO_LOG
